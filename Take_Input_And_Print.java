68c08c0f34b471c1b58d7b0f3045d11fa790924e
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-05 12:01:05 +0530

    import java.util.Scanner; public class Add{          public static void main(String[] args)          {               Scanner Sc = new Scanner(System.in);               String a=Sc.nextLine();               System.out.println(a);          } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Add{
+         public static void main(String[] args)
+         {
+              Scanner Sc = new Scanner(System.in);
+              String a=Sc.nextLine();
+              System.out.println(a);
+         }
+}
