daf2a929bd5b8befd18fb43c7bece2e89f22a95c
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-06 22:49:45 +0530

    import java.util.*; public class Mul{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         int c=s.nextInt();         long d=512*a*b*c*2;         System.out.println(d);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Mul{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        long d=512*a*b*c*2;
+        System.out.println(d);
+    }
+}
