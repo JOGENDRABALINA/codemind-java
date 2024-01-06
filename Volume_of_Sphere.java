a817ce9dba8280fc08688eb31ac61a0cafdf6c05
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-06 17:04:32 +0530

    import java.util.Scanner; public class sphere{          public static void main(String[] args)          {               Scanner s=new Scanner(System.in);               int a=s.nextInt();               double b=1.3333333*3.14*a*a*a;               System.out.printf("%.2f",b);          } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class sphere{
+         public static void main(String[] args)
+         {
+              Scanner s=new Scanner(System.in);
+              int a=s.nextInt();
+              double b=1.3333333*3.14*a*a*a;
+              System.out.printf("%.2f",b);
+         }
+}
