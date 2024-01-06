86197c137468da078e3c1a6725346606e1b693f2
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-06 17:09:17 +0530

    import java.util.Scanner; public class Add{          public static void main(String[] args)          {               Scanner s=new Scanner(System.in);               int a=s.nextInt();               int b=a*a;               int c=4*a;               System.out.printf("%d %d",b,c);          } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Add{
+         public static void main(String[] args)
+         {
+              Scanner s=new Scanner(System.in);
+              int a=s.nextInt();
+              int b=a*a;
+              int c=4*a;
+              System.out.printf("%d %d",b,c);
+         }
+}
