5625dfdae2454d0086bd3eaa2bb9633683292463
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-06 11:54:25 +0530

    import java.util.Scanner; public class Circle{          public static void main(String[] args)          {               Scanner s=new Scanner(System.in);               int a=s.nextInt();               double c=3.14*a*a;               System.out.printf("%.2f",c);          } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Circle{
+         public static void main(String[] args)
+         {
+              Scanner s=new Scanner(System.in);
+              int a=s.nextInt();
+              double c=3.14*a*a;
+              System.out.printf("%.2f",c);
+         }
+}
