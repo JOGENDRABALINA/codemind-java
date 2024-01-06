f64e80464e0ce94e0f0d1f49dbc58fa270e7bb61
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-06 21:43:55 +0530

    import java.util.*; public class Avg{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         int n=(2*a)-b;         System.out.println(n);
        }
    }

---
+++ Find_Second_Number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Avg{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int n=(2*a)-b;
+        System.out.println(n);
+        
+    }
+}
