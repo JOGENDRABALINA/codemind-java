c6ba5b833fe4300ada696cdfbfaf2dbf2d53dd39
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-07 20:59:42 +0530

    import java.util.*; public class Home{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n = s.nextInt();         if (n>98)         {
                System.out.println("YES");
            }
            
            else
            {
                System.out.println("NO");
            }
        }
    }

---
+++ Fever.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Home{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n = s.nextInt();
+        if (n>98)
+        {
+            
+            System.out.println("YES");
+        }
+        
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
