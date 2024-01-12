7a92b289d8e163d24c8a43463831c685d14acf14
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-12 09:25:53 +0530

    import java.util.*; public class Home{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n = s.nextInt();         int i;         int sum=0;         for(i=1;i<=n;i++)         {             sum=sum+i;         }         System.out.println(sum);     } }

---
+++ Sum_of_first_N_natural_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Home{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n = s.nextInt();
+        int i;
+        int sum=0;
+        for(i=1;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
