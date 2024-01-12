5ebb495b3c539f1cd3a16ed22e34230f5c3458f9
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-12 09:56:16 +0530

    import java.util.*; public class Home{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n = s.nextInt();         int m = s.nextInt();         int i;         for(i=n;i<=m;i++)         {            System.out.print(i+" ");         }
        }
    }

---
+++ A_to_B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Home{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int i;
+        for(i=n;i<=m;i++)
+        {
+           System.out.print(i+" ");
+        }
+        
+    }
+}
