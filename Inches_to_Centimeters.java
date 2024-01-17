8543db00690da927e415b6c7a6c466bab811e791
BALINA JOGENDRA VENKATA SIVA SUBRAHMANYAM <121618316+JOGENDRABALINA@users.noreply.github.com>
2024-01-17 23:20:19 +0530

    import java.util.Scanner; public class Add{     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int a=s.nextInt();         System.out.printf("%.2f",a*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Add{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.printf("%.2f",a*2.54);
+    }
+}
